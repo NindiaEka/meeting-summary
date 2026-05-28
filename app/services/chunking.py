6864115a6c41736048ab7517c380875014a7b581
Nindia Ekasuci Larasati (Sisindokom) <NindiaLaSis@unias.com>
2026-05-28 15:41:27 +0700

    update MoM

--- app/services/chunking.py
+++ app/services/chunking.py
@@ -1,11 +1,11 @@
-from langchain_text_splitters import (RecursiveCharacterTextSplitter)
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 import os
 
 def chunk_text(
         text, 
         chunk_size=int(os.getenv("CHUNK_SIZE")),
         chunk_overlap=int(os.getenv("CHUNK_OVERLAP"))):
-    text_splitter = RecursiveCharacterTextSplitter(
+    text_splitter = RecursiveCharacterTextSplitter.from_tiktoken_encoder(
         chunk_size=chunk_size,
         chunk_overlap=chunk_overlap,
         separators=["\n\n", "\n", " ", ""]
